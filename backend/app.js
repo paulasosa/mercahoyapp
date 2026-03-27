713787d35a1784dd7de1c8598776b80f464cd06d
Paula Sosa Hernandez <paulasosahdez@gmail.com>
2026-03-26 22:49:04 -0500

    update notificacion in app.js
    update notificacion in app.js

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola Mundo desde el backend!');
+  res.send('¡Por aqui empieza el backend!');
 });
 
 app.listen(port, () => {
