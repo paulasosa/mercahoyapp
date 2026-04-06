27be615be09b736b45af56e77822ee3b2539e612
Paula Sosa Hernandez <paulasosahdez@gmail.com>
2026-04-05 21:17:43 -0500

    update button

--- frontend/js/search.js
+++ frontend/js/search.js
@@ -1,10 +1,9 @@
-import { Input, initMDB } from "mdb-ui-kit";
+document.addEventListener("DOMContentLoaded", () => {
+  const searchButton = document.getElementById('search-button');
+  const searchInput = document.getElementById('search-input');
 
-initMDB({ Input });
-
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
-searchButton.addEventListener('click', () => {
-  const inputValue = searchInput.value;
-  alert(inputValue);
-}); 
+  searchButton.addEventListener('click', () => {
+    const inputValue = searchInput.value;
+    alert(inputValue);
+  });
+});
