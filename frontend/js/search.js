52caf16b9a43397d93cf459d8fb783f80b207a68
Paula Sosa Hernandez <paulasosahdez@gmail.com>
2026-04-05 21:50:23 -0500

    yujjuuu button search function and layout works too

--- frontend/js/search.js
+++ frontend/js/search.js
@@ -1,9 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
+  console.log("JS cargado");
+
   const searchButton = document.getElementById('search-button');
   const searchInput = document.getElementById('search-input');
 
+  //console.log(searchButton, searchInput);
+  console.log("Botón:", searchButton);
+  console.log("Input:", searchInput);
+
   searchButton.addEventListener('click', () => {
-    const inputValue = searchInput.value;
-    alert(inputValue);
+    //lert("Click funcionando");
+    console.log("CLICK DETECTADO");
+    alert(searchInput.value);
   });
 });
+
