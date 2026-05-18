bc1b6e34776122ba96dbf477703084d9f2733011
Paula Sosa Hernandez <paulasosahdez@gmail.com>
2026-05-18 01:41:30 -0500

    Update authMiddleware.js

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization?.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      res.status(401).json({ message: "Token inválido" });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: "No autorizado" });
+  }
+};
+
+module.exports = { protect }; 
